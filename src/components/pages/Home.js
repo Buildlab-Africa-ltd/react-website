9921b0ea07d379f0919682be7bd2834eeb1b58c4
Ayomikun <96062920+teshyoma@users.noreply.github.com>
2022-11-29 15:10:01 +0100

    Add files via upload

---
+++ src/components/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer'
+import Tracks from '../Tracks'
+
+function Home() {
+    
+    return (
+        <>
+           <HeroSection />
+           <Tracks />
+           <Footer />
+        </>
+    );
+    }
+
+export default Home;
+
+
+
